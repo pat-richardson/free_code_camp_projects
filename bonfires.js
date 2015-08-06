4a0f3eec9e50cb3b5b6de484c18c22b3fb80d2e7
Patrick Richardson <luckysob86@gmail.com>
2015-08-05 20:02:23 -0500

    added code for truncate a string

--- bonfires.js
+++ bonfires.js
@@ -110,3 +110,19 @@ function repeat(str, num) {
 }
 repeat('abc', 5);
 
+//Bonfire: Truncate a string
+function truncate(str, num) {
+  // Clear out that junk in your trunk
+  if(str.length > num){
+    str = str.slice(0,(num - 3)) + '...';
+  }
+  return str;
+}
+
+truncate('A-tisket a-tasket A green and yellow basket', 11);
+
+
+
+
+
+
